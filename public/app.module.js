98cf0b065bc6873bbabec25a1916a92352bdd8af
Airyque <rubyrobinson@Rubys-MacBook-Air.local>
2017-03-07 14:23:23 -0800

    Trying to get routing working

--- public/app.module.js
+++ public/app.module.js
@@ -1,3 +1,27 @@
-angular.module('myTheaterApp', [
-  'movieList'
-]);
+var myTheaterApp = angular.module('myTheaterApp', [
+  'movieList',
+  'ngRoute'
+])
+.config(['$routeProvider', '$locationProvider',
+  function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'movie-list/movie-list.html',
+      controller: 'MovieListController'
+    })
+    .when('/overview', {
+      templateUrl: 'movie-list/movie-list.html',
+      controller: 'MovieListController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+    $locationProvider.html5Mode(true);
+}])
+.controller('MovieListController', ['$route', '$routeParams', '$location',
+  function MovieListController($route, $routeParams, $location) {
+    this.$route = $route;
+    this.$location = $location;
+    this.$routeParams = $routeParams;
+}])
+
