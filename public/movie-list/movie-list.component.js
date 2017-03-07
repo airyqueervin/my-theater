fce1e3b5774db872196fb9cf7ab36280babe0d27
Airyque <rubyrobinson@Rubys-MacBook-Air.local>
2017-03-06 20:04:01 -0800

    Added in some links to watch trailers

--- public/movie-list/movie-list.component.js
+++ public/movie-list/movie-list.component.js
@@ -9,25 +9,30 @@ angular.
     this.movies = [
       {
         name: 'Logan',
-        snippet: 'He is the last Wolverine!',
-        age: 1
+        snippet: 'This is the last Wolverine!',
+        age: 1,
+        url: 'https://www.youtube.com/watch?v=DekuSxJgpbY'
       },
       {
         name: 'Lego-Batman',
-        snippet: 'Always gets it on the first try.',
-        age: 2
+        snippet: 'Always gets it on the first try!',
+        age: 2,
+        url: 'https://www.youtube.com/watch?v=rGQUKzSDhrg'
       },
       {
         name: 'Hidden Figures',
         snippet: 'Fantastic Oscar Worthy!',
-        age: 4
+        age: 4,
+        url: 'https://www.youtube.com/watch?v=RK8xHq6dfAo'
       },
       {
         name: 'John Wick 2',
         snippet: 'Ohhhhh....SNAP!',
-        age: 3
+        age: 3,
+        url: 'https://www.youtube.com/watch?v=ChpLV9AMqm4'
       }
     ];
     this.orderProp = 'age';
+
   }
 });
